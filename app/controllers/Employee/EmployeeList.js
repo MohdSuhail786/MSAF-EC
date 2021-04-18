3c2d6cf17ad24af4dacdfa4bc1f6174479df8684
Mohd Suhail <thesuhailansari786246@gmail.com>
2021-04-18 22:39:44 +0530

    fix in get employee api

--- app/controllers/Employee/EmployeeList.js
+++ app/controllers/Employee/EmployeeList.js
@@ -2,9 +2,8 @@ const User = require('../../models/User')
 
 exports.getEmployees = async (req,res) => {
     try {
-        
         let employees = []
-        employees = await User.findMany({type:2})
+        employees = await User.find({type:2})
         return res.json({employees : employees}).status(200)   
     } catch (err) {
         console.log(err.message)
