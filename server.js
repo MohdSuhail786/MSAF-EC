6ecc2f33604809e7bb1e1f5db2c79c70e0654eec
Mohd Suhail <thesuhailansari786246@gmail.com>
2021-04-28 10:05:35 +0530

    route fixed

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, 'build')))
 //app.set('view engine', 'ejs')
 //app.set("views", path.join(__dirname, "public"))
 
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
