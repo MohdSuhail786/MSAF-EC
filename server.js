c97d9d06b088661be085d39965094a3986be1946
root <root@ip-172-31-16-131.us-east-2.compute.internal>
2021-04-28 04:30:13 +0000

    updated server code

--- server.js
+++ server.js
@@ -7,9 +7,13 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-app.use(express.static(path.join(__dirname, 'public')))
-app.set('view engine', 'ejs')
-app.set("views", path.join(__dirname, "public"))
+app.use(express.static(path.join(__dirname, 'build')))
+//app.set('view engine', 'ejs')
+//app.set("views", path.join(__dirname, "public"))
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 connectToMongoDB()
 
