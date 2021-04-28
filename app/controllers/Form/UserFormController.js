fab7251895afc6de3965815b71c18126db4a791e
Mohd Suhail <thesuhailansari786246@gmail.com>
2021-04-28 21:03:00 +0530

    minor fix

--- app/controllers/Form/UserFormController.js
+++ app/controllers/Form/UserFormController.js
@@ -5,6 +5,7 @@ try{
     req.body.userId = req.user.id
     console.log(req.user)
     if (req.body._id != "") {
+        req.body.userId = req.body.employeeId
         await UserForm.updateOne({_id:req.body._id},req.body)
         return res.json({message: "Form updated"})
     } 
