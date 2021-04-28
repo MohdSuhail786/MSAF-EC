22f5206db2ccf68e534864697725f327fc09b0e7
Mohd Suhail <thesuhailansari786246@gmail.com>
2021-04-28 20:15:51 +0530

    update form support added

--- app/controllers/Form/UserFormController.js
+++ app/controllers/Form/UserFormController.js
@@ -3,7 +3,11 @@ const UserForm = require("../../models/UserForm")
 exports.submitForm = async (req,res) => {
     req.body.userId = req.user.id
     console.log(req.user)
+    if (req.body._id != "") {
+        await UserForm.updateOne({_id:req.body._id},req.body)
+        return res.json({message: "Form updated"})
+    } 
     let userForm = new UserForm(req.body);
     await userForm.save();
-    res.json({message: "Form submitted"})
+    return res.json({message: "Form submitted"})
 }
