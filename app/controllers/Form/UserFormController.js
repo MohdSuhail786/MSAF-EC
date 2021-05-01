b0db89c1a6bc07ffeda02877e059a2d7451e796b
Mohd Suhail <thesuhailansari786246@gmail.com>
2021-05-01 20:20:16 +0530

    fix

--- app/controllers/Form/UserFormController.js
+++ app/controllers/Form/UserFormController.js
@@ -6,6 +6,11 @@ try{
     console.log(req.user)
     if (req.body._id != "") {
         req.body.userId = req.body.employeeId
+        if (req.body.fileName == NULL || req.body.originalFileName == NULL) {
+            let oldForm = await UserForm.find({_id:req.body._id});
+            req.body.fileName = oldForm.fileName;
+            req.body.originalFileName = oldForm.originalFileName;
+        }
         await UserForm.updateOne({_id:req.body._id},req.body)
         return res.json({message: "Form updated"})
     } 
