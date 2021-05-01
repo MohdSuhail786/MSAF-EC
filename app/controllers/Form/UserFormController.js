c0c13e2a2f14b8303fc93f535e0e6c30e777820c
Mohd Suhail <thesuhailansari786246@gmail.com>
2021-05-01 20:24:46 +0530

    fix

--- app/controllers/Form/UserFormController.js
+++ app/controllers/Form/UserFormController.js
@@ -4,19 +4,19 @@ exports.submitForm = async (req,res) => {
 try{
     req.body.userId = req.user.id
     console.log(req.user)
-    if (req.body._id != "") {
+    if (req.body.user_id != "") {
         req.body.userId = req.body.employeeId
         if (req.body.fileName == NULL || req.body.originalFileName == NULL) {
-            let oldForm = await UserForm.find({_id:req.body._id});
+            let oldForm = await UserForm.find({_id:req.body.user_id});
             req.body.fileName = oldForm.fileName;
             req.body.originalFileName = oldForm.originalFileName;
         }
-        await UserForm.updateOne({_id:req.body._id},req.body)
+        await UserForm.updateOne({_id:req.body.user_id},req.body)
         return res.json({message: "Form updated"})
     } 
     let userForm = new UserForm(req.body);
     await userForm.save();
     return res.json({message: "Form submitted"})
 }
-catch(err){console.log(err.message)}
+catch(err){console.log(err.message); console.log("SUHAIL"); console.log(err)}
 }
