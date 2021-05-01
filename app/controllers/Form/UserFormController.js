77a6706a4a8f7d6830bdb355e18c747c84af4883
root <root@ip-172-31-16-131.us-east-2.compute.internal>
2021-05-01 14:51:31 +0000

    fix

--- app/controllers/Form/UserFormController.js
+++ app/controllers/Form/UserFormController.js
@@ -4,14 +4,15 @@ exports.submitForm = async (req,res) => {
 try{
     req.body.userId = req.user.id
     console.log(req.user)
-    if (req.body._id != "") {
+	console.log(req.body.user_id)
+    if (req.body.user_id != "") {
         req.body.userId = req.body.employeeId
-        await UserForm.updateOne({_id:req.body._id},req.body)
+        await UserForm.updateOne({_id:req.body.user_id},req.body)
         return res.json({message: "Form updated"})
     } 
     let userForm = new UserForm(req.body);
     await userForm.save();
     return res.json({message: "Form submitted"})
 }
-catch(err){console.log(err.message)}
+catch(err){console.log(err.message); console.log("SUHAIL"); console.log(err)}
 }
