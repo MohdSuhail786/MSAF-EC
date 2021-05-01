ccee4dcd8c60e46267ce9b059cab9dd800b97bf6
Mohd Suhail <thesuhailansari786246@gmail.com>
2021-05-02 01:15:50 +0530

    filter data fix

--- app/controllers/Employee/GetCSV.js
+++ app/controllers/Employee/GetCSV.js
@@ -1,25 +1,25 @@
 const UserForm = require("../../models/UserForm");
 
-exports.getCsv =async (req,res)=>{
-    
-    list = await UserForm.find({userId:req.body.userId})
-    let i = list.length;
-    let response = []
-    list.forEach(e => {
-        response.unshift({id: i--,...e._doc})
-    });
-    res.json(response,200);
-}
+exports.getCsv = async (req, res) => {
+  list = await UserForm.find({ userId: req.body.userId });
+  let i = list.length;
+  let response = [];
+  list.forEach((e) => {
+    response.unshift({ id: i--, ...e._doc });
+  });
+  res.json(response, 200);
+};
 
-exports.filterData = async (req,res) => {
-    if (!req.body.filterObj) {
-        return res.json({message: "illegal filter object"},200)
-    }
-    list = await UserForm.find(req.body.filterObj)
+exports.filterData = async (req, res) => {
+  try {
+    list = await UserForm.find(req.body.filterObj);
     let i = list.length;
-    let response = []
-    list.forEach(e => {
-        response.unshift({id: i--,...e._doc})
+    let response = [];
+    list.forEach((e) => {
+      response.unshift({ id: i--, ...e._doc });
     });
-    return res.json(response,200);
-}
+    return res.json(response, 200);
+  } catch (e) {
+    console.log(e.message);
+  }
+};
